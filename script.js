02305ef198bf7e2df3b0623c6fd47670ba225e1d
Danilo Borges <daniloborgessilva31@gmail.com>
2023-08-17 00:14:00 -0300

    Iniciado

---
+++ script.js
@@ -0,0 +1,11 @@
+var nome = 'Krishnogard';
+var vida = '20/20 Pv';
+var ataque1 = { nome: 'Ataque Básico', dano: 0, custo: 0 }
+var ataque2 = {}
+var ataque3 = {}
+
+var nomeevento = 'dragas';
+var descricao = '21 PV';
+var narra = 'Dragão usou';
+
+
